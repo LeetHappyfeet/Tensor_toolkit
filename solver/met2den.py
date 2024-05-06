d2931257cee3c1d668aa6db3fcbc8b6ab0519564
LeetHappyfeet <138872496+LeetHappyfeet@users.noreply.github.com>
2024-05-06 03:45:49 -0400

    Update met2den.py
    Fixed some errors to fix format error.

--- solver/met2den.py
+++ solver/met2den.py
@@ -1,5 +1,6 @@
 import numpy as np
-from tools import ricciS, ricciT, c4Inv, einT, einE
+from tools import ricciS, ricciT, einT, einE
+from tools.c4Inv import c4Inv
 
 def met2den(gl, delta=None):
     """
@@ -15,21 +16,24 @@ def met2den(gl, delta=None):
     energy_density : list of lists
         4x4 array. Elements of the array are 4-D matrices of double type, representing the energy density tensor.
     """
+    # Convert gl to numpy array
+    gl_np = np.array(gl)
+    
     # Handle default input arguments
     if delta is None:
         delta = [1, 1, 1, 1]
     
     # Calculate metric tensor inverse
-    gu = c4Inv(gl)
+    gu = c4Inv(gl_np)
     
     # Calculate the Ricci tensor
-    R_munu = ricciT(gu, gl, delta)
+    R_munu = ricciT(gu, gl_np, delta)
     
     # Calculate the Ricci scalar
     R = ricciS(R_munu, gu)
     
     # Calculate Einstein tensor
-    E = einT(R_munu, R, gl)
+    E = einT(R_munu, R, gl_np)
     
     # Calculate Energy density
     energy_density = einE(E, gu)
